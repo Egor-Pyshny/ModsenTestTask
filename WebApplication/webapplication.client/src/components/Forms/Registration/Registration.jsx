4fb04a511cd4d6c3a98ace516b309cf0f3146aa9
Egor251 <egorpyshny@gmail.com>
2024-03-28 22:11:37 +0300

    edit registration page

--- WebApplication/webapplication.client/src/components/Forms/Registration/Registration.jsx
+++ WebApplication/webapplication.client/src/components/Forms/Registration/Registration.jsx
@@ -3,34 +3,48 @@ import './Registration.css'
 export const Registration = () =>{
 
     const handleSubmit = (e) => {
-        console.log(e.target);
         const formDictionary = { ...e.target };
         var data = new Map();
         for (var i = 0; i < 5; i++) {
             data.set(formDictionary[i].name, formDictionary[i].value)
         }
         const myJson = JSON.stringify(Object.fromEntries(data));
-        fetch("http://localhost:5067/api/v1.0/user");
-        console.log("asd");
+        fetch("/api/v1.0/user", {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: myJson
+        })
+            .then(response => {
+                if (response.status == 201) {
+                    console.log('Запрос успешно выполнен');
+                } else {
+                    console.error('Ошибка при выполнении запроса');
+                }
+            })
+            .catch(error => {
+                console.error(error);
+            });
     }
 
     return (
         <form id='registerForm' className="registerСontainer" onSubmit={handleSubmit}>
 
             <label htmlFor="firstName">Имя</label>
-            <input type="text" id="firstName" name='firstName' />
+            <input type="text" id="firstName" name='firstName' required/>
             
             <label htmlFor="secondName">Фамилия</label>
-            <input type="text" id="secondName" name='secondName' />
+            <input type="text" id="secondName" name='secondName' required />
             
             <label htmlFor="thirdName">Отчество</label>
-            <input type="text" id="thirdName" name='thirdName' />
+            <input type="text" id="thirdName" name='thirdName' required />
             
             <label htmlFor="email">Почта</label>
-            <input type="email" id="email" name='email' />
+            <input type="email" id="email" name='email' required />
             
             <label htmlFor="password">Пароль</label>
-            <input type="password" id="password" name='password' />
+            <input type="password" id="password" name='password' required />
 
         </form>
     )
